5596d97dfb376ca322c1a825e4b9480df6bfc589
peter <leepeter019@gmail.com>
2020-10-11 17:02:42 -0700

    Item detail added.

--- src/components/productDetail.js
+++ src/components/productDetail.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import { loadProductById, singleProductClear } from '../actions';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from './loader';
+import styles from '../styles/productDetail.module.css';
 
 function ProductDetail() {
 
@@ -17,13 +19,33 @@ function ProductDetail() {
     }
   }, [id])
 
+  function toPrice(cents) {
+    if(cents%100 === 0) {
+      return `$${cents/100}.00`
+    }else {
+      return `$${Math.round(cents/100)}.${cents%100}`
+    }
+  }
+
   return (
-    <div>
+    <div className={styles.container}>
       {
-        product ? product.status==="loading" ? <Loader height="600px" dotSize="large" /> : 
-        product.error ? <div>Error</div>:
-        <div>{product.name}</div>:
-        null
+        product.status==="loading" ? <Loader height="600px" dotSize="large" /> : 
+        product.error ? <div className={styles.error}>There was an error loading products.</div>:
+        <div className={styles.itemContainer}>
+          <div className={styles.imageContainer}>
+            <div className={styles.itemImage} style={{backgroundImage: `url(${product.image ? product.image : window.location.origin +"/images/noImage.png"}`}}></div>
+          </div>
+          <div className={styles.itemInfo}>
+            <div className={styles.name}>{product.name}</div>
+            <div className={styles.price}>{toPrice(product.price)}</div>
+            <div className={styles.itemButtons}>
+              <button className={styles.button}>Add to cart</button>
+              <Link to={`/purchase/${id}`} className={styles.button}>Buy Now</Link>
+            </div>
+            <div className={styles.description}>{product.description}</div>
+          </div>
+        </div>
       }
     </div>
   )
